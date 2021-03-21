8ce36d13a1759bc21ef28cc8c75602ca8b4c8b35
Milán <info.milannemeth@gmail.com>
2021-03-21 16:45:04 +0100

    create UiSourceBuilder

---
+++ Control/UiSourceBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherService.Entity;
+
+namespace WeatherService.Control
+{
+    class UiSourceBuilder
+    {
+        private readonly Region _local;
+        private readonly Region _remote;
+        public UiSourceBuilder(Region local, Region remote)
+        {
+            _local = local;
+            _remote = remote;
+        }
+
+        public void Build()
+        {
+            var sb = new StringBuilder();
+        }
+    }
+}
