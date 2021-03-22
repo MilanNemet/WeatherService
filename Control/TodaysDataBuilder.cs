9e85501d774b232d2aa11b29c2537d76361934ef
Milán <info.milannemeth@gmail.com>
2021-03-22 20:48:24 +0100

    finish TodaysDataBuilder

--- Control/TodaysDataBuilder.cs
+++ Control/TodaysDataBuilder.cs
@@ -6,14 +6,28 @@ using System.Threading.Tasks;
 
 using WeatherService.Boundary;
 using WeatherService.Entity;
+using WeatherService.Interface;
 
 namespace WeatherService.Control
 {
     class TodaysDataBuilder
     {
-        public TodaysDataBuilder(Region remote, FileManager fm)
-        {
+        private readonly Region _remote;
+        private readonly Forecast[] _todaysWeathers;
 
+        public TodaysDataBuilder(Region remote, Forecast[] todaysWeathers)
+        {
+            _remote = remote;
+            _todaysWeathers = todaysWeathers;
+        }
+
+        public Forecast[] Build()
+        {
+            var todaysWeather = _remote.forecasts.ToList().Find(f => f.date.Date == DateTime.Now.Date);
+            var updated = _todaysWeathers.ToList();
+            updated.Add(todaysWeather);
+
+            return updated.ToArray();
         }
     }
 }
