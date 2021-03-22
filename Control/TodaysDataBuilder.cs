4f0017f9475b5572a6d2f3a0fe7812cd4ebb04fa
Milán <info.milannemeth@gmail.com>
2021-03-22 17:16:12 +0100

    Create class

---
+++ Control/TodaysDataBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherService.Boundary;
+using WeatherService.Entity;
+
+namespace WeatherService.Control
+{
+    class TodaysDataBuilder
+    {
+        public TodaysDataBuilder(Region remote, FileManager fm)
+        {
+
+        }
+    }
+}
